1bb528305e929ad38869719820a57c98673caad9
ushanayana <ushanayana@gmail.com>
2026-02-12 15:08:27 +0530

    variables

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "person1:hiiiiii...,person2"
+echo "person2:hiiiii....how r u,person1"
+echo "person1:iam fine,how is your job person2"
+echo "person2:good,butiam upadating with devopsperson1"
+
