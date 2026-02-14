5a461603ab50fff7e22f318d764c33d4490ec2da
ushanayana <ushanayana@gmail.com>
2026-02-14 05:47:00 +0530

    functions

--- 11-functions.sh
+++ 11-functions.sh
@@ -5,10 +5,10 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 
-R="\e[31m]"
-G="\e[32m]"
-Y="\e[33m]"
-N="\e[0m]"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 VALIDATE(){
     if [ $1 -eq 0 ]
     then 
